3b3ffbae05f1b3e16655ead13cbc832257bf73b3
SwetaLavaniya <sweta.lavaniya@gmail.com>
2021-06-11 00:27:00 +0530

    Included failed test for the rule: If the payment is for a physical product, generate a packing slip for shipping

---
+++ OrderProcessingApplication.BusinessRules/RuleEngine/PaymentRuleEngine.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OrderProcessingApplication.BusinessRules.RuleEngine
+{
+    public class PaymentRuleEngine
+    {
+        public string GenerateSlipForShippingForPhysicalProduct()
+        {
+            throw new NotImplementedException(); 
+        }
+    }
+}
---
+++ OrderProcessingApplication.Tests/RuleEngine/PaymentRuleEngineTests.cs
@@ -0,0 +1,16 @@
+﻿using OrderProcessingApplication.BusinessRules.RuleEngine;
+using Xunit;
+
+namespace OrderProcessingApplication.Tests.RuleEngine
+{
+    public class PaymentRuleEngineTests
+    {
+        [Fact]
+        public void ShouldGeneratePackingSlipForShippingForPhysicalProduct()
+        {
+            PaymentRuleEngine paymentRuleEngine = new PaymentRuleEngine();
+            var result = paymentRuleEngine.GenerateSlipForShippingForPhysicalProduct();
+            
+        }
+    }
+}
