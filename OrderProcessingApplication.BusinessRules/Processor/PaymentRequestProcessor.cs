d17d5ebe068713100a9edeeded63ac80eeea565f
SwetaLavaniya <sweta.lavaniya@gmail.com>
2021-06-11 23:55:11 +0530

    Added code to save payment details to data source so that test can pass.

--- OrderProcessingApplication.BusinessRules/Processor/PaymentRequestProcessor.cs
+++ OrderProcessingApplication.BusinessRules/Processor/PaymentRequestProcessor.cs
@@ -1,4 +1,5 @@
-﻿using OrderProcessingApplication.Domain.Request;
+﻿using OrderProcessingApplication.Domain;
+using OrderProcessingApplication.Domain.Request;
 using OrderProcessingApplication.Domain.Response;
 using OrderProcessingApplication.Repository.Interface;
 using System;
@@ -7,8 +8,11 @@ namespace OrderProcessingApplication.Processor
 {
     public class PaymentRequestProcessor
     {
-        public PaymentRequestProcessor(IPaymentRequestProcessorRepository @object)
+        private readonly IPaymentRequestProcessorRepository _paymentRequestProcessorRepository;
+
+        public PaymentRequestProcessor(IPaymentRequestProcessorRepository PaymentRequestProcessorRepository)
         {
+            _paymentRequestProcessorRepository = PaymentRequestProcessorRepository;
         }
 
         public PaymentResponse ProcessPayment(PaymentRequest request)
@@ -17,6 +21,15 @@ namespace OrderProcessingApplication.Processor
             {
                 throw new ArgumentNullException(nameof(request));
             }
+
+            _paymentRequestProcessorRepository.Save(new PaymentDetails {
+                ProductType = request.ProductType,
+                Name = request.Name,
+                Code = request.Code,
+                Quantity = request.Quantity,
+                Amount = request.Amount
+
+            });
 
             return new PaymentResponse
             {
